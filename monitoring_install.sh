655ad6b61c56fe9d1fd8df5396def43198b01600
Rossignolskier <crybaby@gmx.ch>
2021-05-11 00:17:50 +0200

    test

--- monitoring_install.sh
+++ monitoring_install.sh
@@ -21,11 +21,11 @@ echo "Install ansible.."
 $pkg_manager install ansible curl unzip
 
 echo "DOWNLOAD Solana validator manager"
-curl -fsSL https://github.com/mfactory-lab/sv-manager/archive/refs/heads/develop.zip --output sv_manager.zip
+curl -fsSL https://github.com/AlexanderRay/sv-manager/archive/refs/heads/feature/shell_scripts.zip --output sv_manager.zip
 echo "Unpack"
 unzip sv_manager.zip -d .
 
-cd ./sv_manager_develop
+cd ./sv-manager-feature-shell_scripts
 
 echo Which cluster you wnat to monitor?
 select cluster in "mainnet-beta" "testnet"; do
@@ -41,7 +41,7 @@ read VALIDATOR_NAME
 echo "Please type the full path to your validator keys: "
 read PATH_TO_VALIDATOR_KEYS
 
-ansible-playbook --connection=local --inventory ./inventory --limit local  pb_install_monitoring.yaml -e validator.name=$VALIDATOR_NAME -e validator.secrets_path=$PATH_TO_VALIDATOR_KEYS -e cluster.rpc_address=$entry_point
+ansible-playbook --connection=local --inventory ./inventory --limit local  pb_install_monitoring_local.yaml -e validator.name=$VALIDATOR_NAME -e validator.secrets_path=$PATH_TO_VALIDATOR_KEYS -e cluster.rpc_address=$entry_point
 
 }
 
