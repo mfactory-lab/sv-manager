004f2837c8100f8b73a69debe18842c94870f3f3
Alexander Ray <a.ray@mfactory.ch>
2021-05-12 19:52:03 +0200

    -.

--- install/install_validator.sh
+++ install/install_validator.sh
@@ -45,12 +45,21 @@ done
 
 echo "Please type your validator name: "
 read VALIDATOR_NAME
-echo "Please type the full path to your validator keys: "
-read PATH_TO_VALIDATOR_KEYS
+read -e -p "Please type the full path to your validator keys: " -i "/root/" PATH_TO_VALIDATOR_KEYS
+read -e -p "Enter size of new ram-drive in GB (should be server ram-amount minus 16GB): " -i "48" RAM_DISK_SIZE
+read -e -p "Enter size of server new swap in GB (should be eq to ram-amount): " -i "64" SWAP_SIZE
 
 
-pb_params="{'host_hosts': 'local', 'validator_name':'$VALIDATOR_NAME','secrets_path':'$PATH_TO_VALIDATOR_KEYS', 'rpc_address':'$entry_point'}"
-ansible-playbook --connection=local --inventory ./inventory --limit local  playbooks/pb_install_monitoring_local.yaml -e $pb_params
+pb_params="{'host_hosts': 'local', \
+'validator_name':'$VALIDATOR_NAME', \
+'local': {'secrets_path': '$PATH_TO_VALIDATOR_KEYS', 'flat_path': 'True'}, \
+'rpc_address':'$entry_point', \
+'swap_file_size_gb': '$SWAP_SIZE', \
+'ramdisk_size_gb': '$RAM_DISK_SIZE', \
+'solana_user': 'solana', 'set_validator_info': 'False'
+}"
+
+ansible-playbook --connection=local --inventory ./inventory --limit local  playbooks/pb_install_validator.yaml -e $pb_params -v
 
 echo "### Cleanup install folder ###"
 cd ..
