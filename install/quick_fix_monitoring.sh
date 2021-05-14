87aa0514a9462940aa319fa0fbfe9e7c2389c7be
Alexander Ray <a.ray@mfactory.ch>
2021-05-14 12:21:07 +0200

    - quickfix rpc issue 2

--- install/quick_fix_monitoring.sh
+++ install/quick_fix_monitoring.sh
@@ -6,6 +6,10 @@ echo "###   This script will install and/or reconfigure    ###"
 echo "### telegraf and point it to solana.thevalidators.io ###"
 echo "########################################################"
 
+apt-get install coreutils --yes
+
 sed -i 's/15s/60s/' /etc/telegraf/telegraf.conf
 sed -i 's/30s/60s/' /etc/telegraf/telegraf.conf
+sed -i 's/sudo -i -u/timeout -k 50 45 sudo -i -u/' /etc/telegraf/telegraf.conf
+
 systemctl restart telegraf
