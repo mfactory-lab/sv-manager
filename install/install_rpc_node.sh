208e4d269a95748e345efc2f6015b0f8e30b86ff
Rossignolskier <crybaby@gmx.ch>
2021-12-28 01:14:15 +0100

    removing vote key creation for rpc

--- install/install_rpc_node.sh
+++ install/install_rpc_node.sh
@@ -106,7 +106,7 @@ while [ $# -gt 0 ]; do
    if [[ $1 == *"--"* ]]; then
         param="${1/--/}"
         declare ${param}="$2"
-        echo $1 $2 // Optional to see the parameter:value result
+        #echo $1 $2 // Optional to see the parameter:value result
    fi
 
   shift
