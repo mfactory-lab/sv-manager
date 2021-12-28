7238019b458831fa86e52820e5b5e534046d7a26
Rossignolskier <crybaby@gmx.ch>
2021-12-29 00:57:50 +0100

    fix upload keys for RPC node

--- install/install_rpc_node.sh
+++ install/install_rpc_node.sh
@@ -86,11 +86,6 @@ install_rpc () {
   echo "### 'Uninstall ansible ###"
 
   $pkg_manager remove ansible --yes
-  if [ "$inventory" = "mainnet.yaml" ]
-  then
-    echo "WARNING: solana is ready to go. But you must start it by the hand. Use \"systemctl start solana-validator\" command."
-  fi
-
 
   echo "### Check your dashboard: https://solana.thevalidators.io/d/e-8yEOXMwerfwe/solana-monitoring?&var-server=$VALIDATOR_NAME"
 
