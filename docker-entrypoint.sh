e771cabd3aa9a74aa40f8fd69a92eaf46aaef571
Makhonin Vitaliy <makhonin.vit@gmail.com>
2023-08-10 11:29:20 +0300

    Fix group_vars

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 [ "$(ls -A /app/ansible/)" ] && cp -arf /app/ansible/* /ansible || echo "App ansible is empty"
-cp -r /app/ansible/inventory/group_vars /etc/sv_manager/
+cp -r /ansible/inventory/group_vars /etc/sv_manager/
 exec $@
-rm -rf /etc/sv_manager/group_vars
