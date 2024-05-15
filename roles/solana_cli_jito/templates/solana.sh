53421bcab9befbbcec1aed93d207f74efa65a0ce
Makhonin Vitaliy <makhonin.vit@gmail.com>
2024-05-15 09:20:14 +0300

    Fix

--- roles/solana_cli_jito/templates/solana.sh
+++ roles/solana_cli_jito/templates/solana.sh
@@ -1,4 +1,4 @@
 
-if [ -d "{{ solana_bin_path }}/active_release/bin" ] ; then
-  export PATH="$PATH:{{ solana_bin_path }}/active_release/bin"
+if [ -d "{{ solana_app_path }}/active_release/bin" ] ; then
+  export PATH="$PATH:{{ solana_app_path }}/active_release/bin"
 fi
